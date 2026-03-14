1d78ae56dd8d5de799d0abac9b215e98a7f9eb2c
Tero Maakivi <maakivi@gmail.com>
2026-03-14 06:56:46 +0200

    prototype.js edits, added shadowing, README.md edits

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
 # Ohjelmistosuunnittelumalli: Prototype
 
 Tero Maakivi  
+2407546  
 AIC24SP
 
-Ohjelmistosuunnittelumalli: Prototype esimerkkikoodeja  
-inherit kansiossa: C#  
-prototype_js kansiossa: JavaScript  
+## Ohjelmistosuunnittelumalli: Prototype esimerkkikoodeja
+
+#### inherit kansiossa:
+
+C# koodi, joka näyttää miten perinteinen periytyminen toimii C# ja vastaavissa kielissä. Erittäin barebones esimerkki.
+
+#### prototype_js kansiossa:
+
+Varsinainen JavaScript-koodi, joka näyttää prototype luontimallin toimivuuden.
+
+### Ohjelman suoritus:
+
+cd prototype_js  
+node prototype.js
+
+### Ohjelman toiminta lyhyesti
+
+Esimerkkikoodi näyttää miten JavaScriptissä luodaan olio, ja miten sitä käytetään prototyyppinä kun luodaan uusi olio,
+josta taas luodaan kolmas olio:  
+puppy -> doggy -> animal  
+puppyn prototyyppi on doggy, jonka prototyyppi on animal
+
+Olioille lisäillään ohjelman suorituksen aikana metodeja sekä ominaisuuksia, lisäksi lopuksi käydään läpi miten shadowing toimii,
+antamalla prototyyppiketjun keskimmäiselle doggy oliolle oma legs ominaisuus ja se poistetaan myöhemmin.
--- prototype_js/prototype.js
+++ prototype_js/prototype.js
@@ -1,12 +1,16 @@
 // luodaan alkuperäinen animal-olio ja sen metodi
 const animal = {
   animal_sound() {
-    console.log(this.name + " pitää ääntä:");
+    console.log(this.name + " pitää ääntä.");
   },
 };
 
+// animal oliolla ei ole vielä nimi ominaisuutta, joten undefined
+animal.animal_sound();
+
 // luodaan doggy-olio, joka käyttää prototyyppinään animal-oliota
 // .create() luo prototyyppi linkin, .assign() tekisi vain kopion
+
 const doggy = Object.create(animal);
 
 // lisätään doggy-oliolle nimi-ominaisuus ja oma ääni-metodi
@@ -23,20 +27,21 @@ doggy.doggy_sound();
 console.log("Onko animal sama kuin doggy?");
 console.log(animal === Object.getPrototypeOf(doggy));
 
-// muokataan alkuperäistä animal-oliota
+// muokataan alkuperäistä animal-oliota, tässä siis lisätään ylimmälle prototyypille uusi metodi
 animal.animal_sound2 = function () {
   console.log(this.name + ": Meow!");
 };
 // lisätään animal olioon legs ominaisuus
 animal.legs = 4;
 
-// doggy-oliolla on kaikki vanhat ja vain animal-olioon lisätyt ominaisuudet ja metodit
+// jos alussa olisi luotu kopio, alkuperäiselle prototyypille tehdyt muutokset eivät näkyisi
+// doggy-oliolla on nyt kaikki vanhat ja vain animal-olioon lisätyt ominaisuudet ja metodit
 doggy.animal_sound();
 doggy.doggy_sound();
 doggy.animal_sound2();
-console.log(doggy.legs);
+console.log(doggy.name + " oliolla on " + doggy.legs + " jalkaa.");
 // ears ominaisuutta ei ole määritelty -> undefined
-console.log(doggy.ears);
+console.log(doggy.name + " oliolla on " + doggy.ears + " korvaa.");
 
 // luodaan lopuksi puppy-olio jonka prototyyppi on doggy
 const puppy = Object.create(doggy);
@@ -49,4 +54,40 @@ puppy.animal_sound();
 puppy.doggy_sound();
 puppy.animal_sound2();
 puppy.puppy_sound();
-console.log(puppy.legs);
+console.log(puppy.name + " oliolla on " + puppy.legs + " jalkaa.");
+
+console.log("\nSHADOWING");
+console.log("Doggy oliolle annetaan legs-ominaisuus");
+// doggy oli onnettomuudessa ja sille annettiin oma legs ominaisuus, joka varjostaa/shadowing/(ylikirjottaa)
+// prototyypiltä saadun legs = 4 ominaisuuden arvon
+doggy.legs = 3;
+
+// animal oliolla on edelleen 4 jalkaa, mutta doggy ja sitä prototyyppinään käyttävä puppy
+// ovat nyt 3 jalkaisia
+console.log(animal.name + " oliolla on " + animal.legs + " jalkaa.");
+console.log(doggy.name + " oliolla on " + doggy.legs + " jalkaa.");
+console.log(puppy.name + " oliolla on " + puppy.legs + " jalkaa.");
+
+// näyttää onko ominaisuus OMA vai peritty prototyypiltä
+console.log(
+  doggy.name + " oliolla on legs ominaisuus: " + doggy.hasOwnProperty("legs"),
+); // true  — doggy omistaa sen itse
+console.log(
+  puppy.name + " oliolla on legs ominaisuus: " + puppy.hasOwnProperty("legs"),
+); // false — puppy lukee doggy:ltä
+
+console.log("Poistetaan doggy oliolta legs-ominaisuus");
+// doggyn oma
+delete doggy.legs;
+
+console.log(animal.name + " oliolla on " + animal.legs + " jalkaa.");
+console.log(doggy.name + " oliolla on " + doggy.legs + " jalkaa.");
+console.log(puppy.name + " oliolla on " + puppy.legs + " jalkaa.");
+
+// näyttää onko ominaisuus OMA vai peritty prototyypiltä
+console.log(
+  doggy.name + " oliolla on legs ominaisuus: " + doggy.hasOwnProperty("legs"),
+); // true  — doggy omistaa sen itse
+console.log(
+  puppy.name + " oliolla on legs ominaisuus: " + puppy.hasOwnProperty("legs"),
+); // false — puppy lukee doggy:ltä
